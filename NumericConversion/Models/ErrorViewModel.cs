1506fd1560a6338954e648b195d9800b758dfc3f
j.halla@aspenware.com <j.halla@aspenware.com>
2019-09-10 20:49:38 -0600

    Add project files.

---
+++ NumericConversion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NumericConversion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
